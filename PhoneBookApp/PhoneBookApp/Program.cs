5bef081a64babd2e953f1a3e2ac21481c9cf988e
andrea-zecevic <azecev01@gmail.com>
2023-11-23 12:48:44 +0100

    Third commit: Menu functionalities delete contact and edit preference

--- PhoneBookApp/PhoneBookApp/Program.cs
+++ PhoneBookApp/PhoneBookApp/Program.cs
@@ -5,11 +5,14 @@ using System.Collections.Generic;
 
 namespace PhoneDirectory
 {
+    static class PhoneDirectoryManager
+    {
+        public static Dictionary<Contact, List<Call>> PhoneDirectory { get; } = new Dictionary<Contact, List<Call>>();
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Dictionary<Contact, List<Call>> phoneDirectory = new Dictionary<Contact, List<Call>>();
 
             Call call1 = new Call(DateTime.Now.AddHours(-2), CallStatus.Completed);
             Call call2 = new Call(DateTime.Now.AddHours(-9), CallStatus.Completed);
@@ -19,15 +22,15 @@ namespace PhoneDirectory
             Contact contact2 = new Contact("Pera Peric", 0981654321, ContactPreference.Normal);
             Contact contact3 = new Contact("Mara Maric", 0981234567, ContactPreference.Blocked);
 
-            phoneDirectory[contact1] = new List<Call> { call1, call2 };
-            phoneDirectory[contact2] = new List<Call> { call2 };
-            phoneDirectory[contact3] = new List<Call> { call3, call2};
+            PhoneDirectoryManager.PhoneDirectory[contact1] = new List<Call> { call1, call2 };
+            PhoneDirectoryManager.PhoneDirectory[contact2] = new List<Call> { call2 };
+            PhoneDirectoryManager.PhoneDirectory[contact3] = new List<Call> { call3, call2};
 
             var exit = true;
             while (exit)
             {
 
-                Console.WriteLine("\tTelefonski imenik\n\nUnesi broj za odabir:\n1 - Ispis svih kontakata\n" +
+                Console.WriteLine("\n\tTelefonski imenik\n\nUnesi broj za odabir:\n1 - Ispis svih kontakata\n" +
                     "2 - Dodavanje novih kontakata u imenik\n3 - Brisanje korisnika iz imenika\n" +
                     "4 - Editiranje preference kontakta\n5 - Upravljanje kontaktom i poddomena\n" +
                     "6 - Ispis svih poziva\n7 - Izlaz iz aplikacije ");
@@ -39,16 +42,16 @@ namespace PhoneDirectory
                     switch (choice)
                     {
                         case 1:
-                            DisplayContacts(phoneDirectory);
+                            DisplayContacts();
                             break;
                         case 2:
-                            AddContact(phoneDirectory);
+                            AddContact();
                             break;
                         case 3:
-                            //DeleteContact();
+                            DeleteContact();
                             break;
                         case 4:
-                            //EditContact()
+                            EditContactPreference();
                             break;
                         case 5:
                             //MenageContact()
@@ -69,27 +72,27 @@ namespace PhoneDirectory
                 }
             }
 
-            static void DisplayContacts(Dictionary<Contact, List<Call>> phoneBook)
+            static void DisplayContacts()
             {
                 Console.WriteLine("Popis svih kontakata:");
-                foreach (var contact in phoneBook.Keys)
+                foreach (var contact in PhoneDirectoryManager.PhoneDirectory.Keys)
                 {
                     Console.WriteLine($"\nIme i prezime: {contact.FirstAndLastName}, Broj mobitela: {contact.PhoneNumber}, Preferenca: {contact.Preference}");
                     Console.WriteLine("\nPozivi:");
 
-                    foreach (var call in phoneBook[contact])
+                    foreach (var call in PhoneDirectoryManager.PhoneDirectory[contact])
                     {
                         Console.WriteLine($"Vrijeme poziva: {call.CallTime}, Status: {call.Status}");
                     }
                 }
             }
 
-            static void AddContact(Dictionary<Contact, List<Call>> phoneDirectory)
+            static void AddContact()
             {
                 Console.WriteLine("Unesite ime i prezime novog kontakta:");
                 var firstAndLastName = Console.ReadLine();
 
-                if (IsValidName(firstAndLastName))
+                if (!string.IsNullOrWhiteSpace(firstAndLastName) && IsValidName(firstAndLastName))
                 {
                     Console.WriteLine("Unesite broj mobitela:");
 
@@ -100,9 +103,9 @@ namespace PhoneDirectory
                         {
                             Contact newContact = new Contact(firstAndLastName, phoneNumber, preferenca);
 
-                            if (!phoneDirectory.ContainsKey(newContact))
+                            if (!PhoneDirectoryManager.PhoneDirectory.ContainsKey(newContact))
                             {
-                                phoneDirectory[newContact] = new List<Call>();
+                                PhoneDirectoryManager.PhoneDirectory[newContact] = new List<Call>();
                                 Console.WriteLine("Novi kontakt dodan.");
                             }
                             else
@@ -128,8 +131,79 @@ namespace PhoneDirectory
 
             static bool IsValidName(string name) 
             {
-                return !string.IsNullOrWhiteSpace(name) && name.Length >= 3 && name.All(char.IsLetter);
+                return !string.IsNullOrWhiteSpace(name) && name.Length >= 3 && name.All(c => char.IsLetter(c) || char.IsWhiteSpace(c));
             }
+
+            static void DeleteContact()
+            {
+                Console.WriteLine("\nUnesite ime kontakta kojeg zelite izbrisati:");
+                var contactNameToDelete = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(contactNameToDelete))
+                {
+                    var contactToDelete = PhoneDirectoryManager.PhoneDirectory.Keys.FirstOrDefault(contact => contact.FirstAndLastName == contactNameToDelete);
+                    
+                    if (contactToDelete != null)
+                    {
+                        PhoneDirectoryManager.PhoneDirectory.Remove(contactToDelete);
+                        Console.WriteLine($"\nKontakt {contactToDelete.FirstAndLastName} ste uspjesno izbrisali iz imenika.");
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Uneseni kontakt ne postoji u imeniku.");
+                    }               
+                } 
+                else
+                {
+                    Console.WriteLine("Uneseno ime nije ispravno.");
+                }
+            }
+
+            static void EditContactPreference()
+            {
+                Console.WriteLine("Kojem kontaktu zelis izmjeniti preferencu:");
+                var contactToEditPreference = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(contactToEditPreference))
+                {
+
+                    checkIfContactExistsAndChangeItsPreference(contactToEditPreference);
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Uneseno ime nije ispravno.");
+                }
+            }
+
+            static void checkIfContactExistsAndChangeItsPreference(string userInput)
+            {
+                var contactToEdit = PhoneDirectoryManager.PhoneDirectory.Keys.FirstOrDefault(contact => contact.FirstAndLastName == userInput);
+
+                if (contactToEdit != null)
+                {
+                    Console.WriteLine($"Trenutna preferenca {contactToEdit.FirstAndLastName} je {contactToEdit.Preference}");
+
+                    Console.WriteLine("Unesite novu preferencu za kontakt (0 - Favorite, 1 - Normal, 2 - Blocked):");
+
+                    if (Enum.TryParse(Console.ReadLine(), out ContactPreference newPreference) && newPreference < ContactPreference.Blocked)
+                    {
+                        contactToEdit.Preference = newPreference;
+                        Console.WriteLine($"\nKontaktu {contactToEdit.FirstAndLastName} ste uspjesno promjenili preferencu.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Neispravan unos preference.");
+                    }                   
+                }
+                else
+                {
+                    Console.WriteLine("Uneseni kontakt ne postoji u imeniku.");
+                }
+            }
+
+
+
 
 
         }
